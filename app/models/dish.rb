017e9702cf56926fac83a38425950633a4d420e1
changrh <changruohan@gmail.com>
2014-09-15 17:45:24 +0800

    Constraint Added in Model

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,4 @@
 class Dish < ActiveRecord::Base
   has_many :orders, inverse_of: :dish
+  validates :name, :price, :stock, :restaurant, :restaurantURL, :imageURL, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,12 @@
 class Order < ActiveRecord::Base
     belongs_to :dish
+    validates :dish, presence: true
+    validates :phone, presence: true
+    validates :phone, format: { with: /[0-9]{8}/, message: "Not a valid HK Phone Number" }
+    validate :order_amount_cannot_exceeds_dish_stock
+    def order_amount_cannot_exceeds_dish_stock
+      if dish.orders.size > dish.stock
+        errors[:base] << "This dish has been sold out !"
+      end
+    end
 end
